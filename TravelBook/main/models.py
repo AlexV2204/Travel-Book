8262f8a27eeca849a766cc05e65534d5ec777692
AlexV2204 <alexandre.vilbert1@gmail.com>
2020-10-23 12:57:23 +0200

    main page - ?responsive

--- TravelBook/main/models.py
+++ TravelBook/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Voyage(models.Model):
+    destination = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    categorie = models.CharField(max_length=100)
+    contenu = models.CharField(max_length=10000)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.destination
